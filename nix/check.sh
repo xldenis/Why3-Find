7819803e5de9eb82061dd90f2b7bd8b07f499dd6
Loïc Correnson <loic.correnson@cea.fr>
2022-12-01 15:27:25 +0100

    [lint] headers

--- nix/check.sh
+++ nix/check.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Check linting
+# Check indentation
 
-for f in src/*.ml src/*.mli
+echo "Check indent"
+for f in src/*.ml src/*.mli utils/*.ml utils/*.mli ppx/*.ml ppx/*.mli
 do
     ocp-indent $f -o $f.lint
     if diff -q $f $f.lint ;
@@ -13,3 +14,22 @@ do
         exit 1
     fi
 done
+
+# Check headers
+
+echo "Check headers"
+for f in src/*.ml src/*.mli src/*.mll utils/*.ml utils/*.mli ppx/*.ml ppx/*.mli
+do
+    cp $f $f.head
+    headache -h HEADER $f
+    if diff -q $f $f.head ;
+    then
+        rm -f $f.head
+    else
+        echo "File $f has incorrect header"
+        exit 1
+    fi
+done
+
+# Done
+echo "Done."
